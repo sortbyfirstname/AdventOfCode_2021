001b93a79be55affc63afa8182b7561f11ea654e
Aaron Seabrook <mail@aaronseabrook.com>
2021-12-03 00:28:57 +0000

    Day 2: Part 2

--- day2/day2.hs
+++ day2/day2.hs
@@ -1,17 +1,18 @@
 -- Part 1
 
-type Position = (Int, Int)
+type Position = (Int, Int, Int)
 
 update :: Position -> [String] -> Position
-update (horiz, depth) [command, value]
-    | command == "forward" = (horiz + (read $ value), depth)
-    | command == "down" = (horiz, depth + (read $ value))
-    | command == "up" = (horiz, depth - (read $ value))
-    | otherwise = (0,0)
+update (horiz, depth, aim) [command, number]
+    | command == "forward" = (horiz + units, depth + aim * units, aim)
+    | command == "down" = (horiz, depth, aim + units)
+    | command == "up" = (horiz, depth, aim - units)
+    | otherwise = (0,0,0)
+    where units = read $ number
 
 part1 :: IO ()
 part1 = do
     input <- readFile "day2_input.txt"
     let contents = map words $ lines input
-    let (depth, horizontal) = foldl update (0,0) contents
-    print $ depth * horizontal
+    let (horizontal, depth, aim) = foldl update (0,0,0) contents
+    print $ horizontal * depth
